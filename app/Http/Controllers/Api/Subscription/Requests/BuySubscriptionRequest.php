e7b931be8992a6e1618c7e5c27fd50189474ea5b
Usman A. <usmananwar916@gmail.com>
2024-09-06 15:02:30 +0500

    PTF: implementing subscriptions

---
+++ app/Http/Controllers/Api/Subscription/Requests/BuySubscriptionRequest.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Http\Controllers\Api\Subscription\Requests;
+
+use App\Http\Requests\BaseRequest;
+
+
+class BuySubscriptionRequest extends BaseRequest
+{
+    public function rules()
+    {
+        return [];
+    }
+}
